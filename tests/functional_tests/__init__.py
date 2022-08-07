17bca36643b21f5fc7c9121dc0a36c0f3fa1df0a
Evgeniy <trofimovevgeniy9@gmail.com>
2022-08-07 09:58:44 +0300

    Проверка возможности ввода элемента списка

---
+++ tests/functional_tests/__init__.py
@@ -0,0 +1,6 @@
+import unittest
+
+from tests.functional_tests.home_page import NewVisitorTest
+
+if __name__ == '__main__':
+    unittest.main()
--- tests/functional_tests/home_page.py
+++ tests/functional_tests/home_page.py
@@ -1,4 +1,8 @@
+import time
+
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 
@@ -23,22 +27,33 @@ class NewVisitorTest(unittest.TestCase):
         # Название вкладки
         self.assertIn('To-Do', self.browser.title)
 
+        # Пользователь видит, что заголовок и шапка страницы говорят о списках дел
+        header_text = self.browser.find_element(by=By.TAG_NAME, value='h1').text
+        self.assertIn('To-Do', header_text)
 
+        # Предложение ввода элемента списка
+        input_box = self.browser.find_element(by=By.ID, value='id_new_item')
+        self.assertEqual(input_box.get_attribute('placeholder'), 'Enter a to-do item')
 
-# Предложение ввода элемента списка
+        # Ввод в текстовом поле 'Создать тестовое дело'
+        input_box.send_keys('Создать тестовое дело')
 
-# Ввод в текстовом поле 'Создать тестовое дело'
+        # При нажатии на enter страница обновляется
+        # И содержит: '1: Создать тестовое дело'
+        input_box.send_keys(Keys.ENTER)
+        time.sleep(1)
 
-# При нажатии на enter страница обновляется
-# И содержит: '1: Создать тестовое дело'
+        table = self.browser.find_element(by=By.ID, value='id_list_table')
+        rows = table.find_elements(by=By.TAG_NAME, value='tr')
+        self.assertTrue(
+            any(row.text == '1: Создать тестовое дело' for row in rows)
+        )
 
-# Текстовое поле доступно. Вводим: 'Создать тестовое дело 2'
+        # Текстовое поле доступно. Вводим: 'Создать тестовое дело 2'
+        self.fail('Закончить тест')
 
 # Страница обновляется и содержит 2 элемента
 
 # Сайт сгенерировал уникальный url-адрес
 
 # Переход по адресу - список на месте
-
-if __name__ == '__main__':
-    pass
