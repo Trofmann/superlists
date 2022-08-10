84fe3a63b84a92d0de1c7c0a68b44a668fbee9b0
Evgeniy <trofimovevgeniy9@gmail.com>
2022-08-10 20:54:40 +0300

    Лишние импорты

--- lists/tests.py
+++ lists/tests.py
@@ -1,8 +1,4 @@
-from django.http import HttpRequest
 from django.test import TestCase
-from django.urls import resolve
-from .views import home_page
-from django.template.loader import render_to_string
 
 
 class HomePageTest(TestCase):
