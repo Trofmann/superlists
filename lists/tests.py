438ec7cd0b9925baaddbbc928bcf03c3626abed4
Evgeniy <trofimovevgeniy9@gmail.com>
2022-11-12 23:44:30 +0300

    Страница 116: Переадресация после post И показ всех элементов в таблице

--- lists/tests.py
+++ lists/tests.py
@@ -12,16 +12,48 @@ class HomePageTest(TestCase):
         Тест: используется шаблон домашней страницы
         """
         response = self.client.get('/')
+
         self.assertTemplateUsed(response, 'lists/home.html')
 
     def test_can_save_a_POST_request(self):
         """
         Тест: можно сохранить post-запрос
         """
+        self.client.post('/', data={'item_text': 'A new list item'})
+
+        new_item = Item.objects.first()  # type: Item
+
+        self.assertEqual(Item.objects.count(), 1)
+        self.assertEqual(new_item.text, 'A new list item')
+
+    def test_redirects_after_POST(self):
+        """
+        Тест: переадресует после post-запроса
+        """
         response = self.client.post('/', data={'item_text': 'A new list item'})
 
-        self.assertIn('A new list item', response.content.decode())
-        self.assertTemplateUsed(response, 'lists/home.html')
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], '/')
+
+    def test_only_saves_items_when_necessary(self):
+        """
+        Тест: сохраняет элементы только когда нужно
+        """
+        self.client.get('/')
+
+        self.assertEqual(Item.objects.count(), 0)
+
+    def test_displays_all_list_items(self):
+        """
+        Тест: отображаются все элементы списка
+        """
+        Item.objects.create(text='itemey 1')
+        Item.objects.create(text='itemey 2')
+
+        response = self.client.get('/')
+
+        self.assertIn('itemey 1', response.content.decode())
+        self.assertIn('itemey 2', response.content.decode())
 
 
 class ItemModelTest(TestCase):
@@ -42,9 +74,9 @@ class ItemModelTest(TestCase):
         second_item.save()
 
         saved_items = Item.objects.all()
-        self.assertEqual(saved_items.count(), 2)
-
         first_saved_item = saved_items[0]
         second_saved_item = saved_items[1]
+
+        self.assertEqual(saved_items.count(), 2)
         self.assertEqual(first_saved_item.text, 'The first (ever) list item')
         self.assertEqual(second_saved_item.text, 'Item the second')
--- lists/views/home_page.py
+++ lists/views/home_page.py
@@ -1,10 +1,16 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from lists.models import Item
 
 
 def home_page(request):
     """Домашняя страница"""
+    if request.method == 'POST':
+        Item.objects.create(text=request.POST['item_text'])
+        return redirect('/')
+
+    items = Item.objects.all()
     context = {
-        'new_item_text': request.POST.get('item_text', ''),
+        'items': items
     }
     return render(request=request, template_name='lists/home.html', context=context)
