33928a2a3c39ddd23d640adda7d6eca993b6760a
Evgeniy <trofimovevgeniy9@gmail.com>
2022-11-16 17:51:32 +0300

    Страница 188 Интеграция bootstrap

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,13 +1,13 @@
 import time
 
-from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.common import WebDriverException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
     """
     Тест нового пользователя
     """
