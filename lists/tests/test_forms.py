13f6376cd5e49b9c45cee447a687d9473717d3f4
Evgeniy <trofimovevgeniy9@gmail.com>
2023-06-18 21:12:12 +0300

    Страница 311 Форма добавления в существующий список

--- lists/tests/test_forms.py
+++ lists/tests/test_forms.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 
-from ..forms import ItemForm, EMPTY_ITEM_ERROR
+from ..forms import EMPTY_ITEM_ERROR, DUPLICATE_ITEM_ERROR, ExistingListItemForm, ItemForm
 from ..models import List, Item
 
 
@@ -36,3 +36,37 @@ class ItemFormTest(TestCase):
         self.assertEqual(new_item, Item.objects.first())
         self.assertEqual(new_item.text, 'do me')
         self.assertEqual(new_item.list, list_)
+
+
+class ExistingListItemFormTest(TestCase):
+    """Тест формы элемента существующего списка"""
+
+    def test_form_renders_item_text_input(self):
+        """
+        Тест: форма отображает текстовый ввод элемента
+        """
+        list_ = List.objects.create()
+        form = ExistingListItemForm(for_list=list_)
+        self.assertIn('placeholder="Enter a to-do item"', form.as_p())
+
+    def test_form_validation_for_blank_items(self):
+        """
+        Тест: валидация формы для пустых элементов
+        """
+        list_ = List.objects.create()
+        data = {'text': ''}
+        form = ExistingListItemForm(for_list=list_, data=data)
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors['text'], [EMPTY_ITEM_ERROR])
+
+    def test_form_validation_for_duplicate_items(self):
+        """
+        Тест: валидация формы для повторяющихся элементов
+        """
+        text = 'no twins!'
+        list_ = List.objects.create()
+        Item.objects.create(list=list_, text=text)
+
+        form = ExistingListItemForm(for_list=list_, data={'text': text})
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors['text'], [DUPLICATE_ITEM_ERROR])
--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -1,3 +1,5 @@
+from unittest import skip
+
 from django.test import TestCase
 from django.utils.html import escape
 
@@ -144,6 +146,24 @@ class ListViewTest(TestCase):
         """
         response = self.post_invalid_input()
         self.assertContains(response, escape(EMPTY_ITEM_ERROR))
+
+    @skip
+    def test_duplicate_item_validation_errors_end_up_on_page(self):
+        """
+        Тест: ошибки валидации повторяющегося элемента оканчиваются на странице списков
+        """
+        text = 'textey'
+        list1 = List.objects.create()
+        item1 = Item.objects.create(list=list1, text=text)
+        response = self.client.post(
+            f'/lists/{list1.id}/',
+            data={'text': text}
+        )
+
+        expected_error = escape("You've already got this in your list")
+        self.assertContains(response, expected_error)
+        self.assertTemplateUsed(response, 'lists/list.html')
+        self.assertEqual(Item.objects.count(), 1)
 
 
 class NewListTest(TestCase):
