f006585e1e4b92b7c7b65c1b4efa92ded3e973d6
Evgeniy <trofimovevgeniy9@gmail.com>
2023-06-27 22:44:11 +0300

    Страница 508 Добавление в список тех, с кем поделились

--- lists/views/share.py
+++ lists/views/share.py
@@ -6,4 +6,7 @@ from lists.models import List
 def share_list(request, list_id):
     """Поделиться"""
     list_ = List.objects.get(id=list_id)
+    if request.method == 'POST':
+        email = request.POST['email']
+        list_.shared_with.add(email)
     return redirect(list_)
